b11326cab5ac9e54d736a0ab0651254d372c6f89
Alex Arenols Solano <depruebas@MacBook-Pro-de-Alex.local>
2023-08-11 05:58:33 +0200

    Changes 02 folder

--- 02/config/ConfigClass.py
+++ 02/config/ConfigClass.py
@@ -1,30 +1,48 @@
+# Importamos las clase que vamos a utilizar para leer los ficheros
+# de configuración, en este caso .INI y .JSON
 import configparser
 import json
 
 class ConfigClass:
 
-  def Get( file, section, value):
+  # Método para leer los datos del un fichero de configuración.
+  # Recibe los parametros:
+  #
+  #     file: fichero de configuración: formato = 'path/config.ini'
+  #     section: sección del fichero de configuración, en el ejemplo database o logs
+  #     key: Clave de la que queremos obtener su valor, ejem.: username
+  #
+  # Devuelve el valor asociado a la seccion/clave o un error.
+  #
+  def Get( file, section, key):
 
+    # Obtenemos la extensión para saber el tipo de fichero a procesar
     config_file = file.split(".")
 
+    # Si el fichero es tipo .INI
     if config_file[1] == 'ini':
+      # Cargamos el ConfigParser y leemos el fichero
       config = configparser.ConfigParser()
       config.read(file)
 
+      # Obtenemos el valor de la clave pasado y lo encerramos en un 
+      # try ... except para poder tratar el error si no existe la clave o seccion
       try: 
-        return config.get( section, value) 
+        return config.get( section, key) 
       except: 
         return "nothing1" 
     
+    # hacemos lo mismo con los fichero .json
     elif config_file[1] == 'json':
       with open( file) as config_json_file:
         config = json.load( config_json_file)
       
       try:
-        return config[ section][ value]
+        return config[ section][ key]
       except: 
         return "nothing2" 
     
+    # Devolvemos un error o lo que queramos si el fichero de configuracion no existe
     else:
       return "nothing3"
 
--- 02/init.py
+++ 02/init.py
@@ -1,11 +1,17 @@
+# Incluimos nuestro modulo para leer ficheros de configuración
 from config.ConfigClass import ConfigClass
 
+# Leemos los datos que queremos del fichero de configuración con la
+# clase ConfigClass y el método Get, los parametros que se pasan son
+# el fichero de configuración, la sección y la clave del valor que 
+# queremos recuperar.
+print( ConfigClass.Get ( 'config/config.ini', 'database', 'host'))
+print( ConfigClass.Get ( 'config/config.ini', 'database', 'username'))
 
-print( ConfigClass.Get ( 'config.ini', 'database', 'host'))
-print( ConfigClass.Get ( 'config.ini', 'database', 'username'))
+print( ConfigClass.Get ( 'config/config.json', 'database', 'password'))
+print( ConfigClass.Get ( 'config/config.json', 'logs', 'perro'))
 
-print( ConfigClass.Get ( 'config.json', 'database', 'password'))
-print( ConfigClass.Get ( 'config.json', 'logs', 'perro'))
-
-
+# comprobamos resultado si ponemos un fichero de configuracion que no existe
 print( ConfigClass.Get ( 'config.txt', 'logs', 'path_app'))
+
+
